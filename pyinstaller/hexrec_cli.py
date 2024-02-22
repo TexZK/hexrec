533ceca282aa97a1de33e1a16badd83f01969b61
Andrea Zoppi <texzk@email.it>
2024-02-22 20:13:04 +0100

    Removed pyinstaller wrapper
    Signed-off-by: Andrea Zoppi <texzk@email.it>

--- pyinstaller/hexrec_cli.py
+++
@@ -1,7 +0,0 @@
-"""
-Run PyInstaller against this script file to build a standalone executable.
-Make sure that hexrec is installed into the Python environment before.
-"""
-from hexrec.__main__ import main as _main
-
-_main('__main__')
